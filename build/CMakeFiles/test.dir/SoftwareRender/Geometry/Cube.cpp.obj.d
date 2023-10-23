7a0466314d0f94ff1104440606d7f157c1f63270
KasumiUtaha <144312278+KasumiUtaha@users.noreply.github.com>
2023-10-23 16:59:06 +0800

    Delete build directory

--- build/CMakeFiles/test.dir/SoftwareRender/Geometry/Cube.cpp.obj.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/test.dir/SoftwareRender/Geometry/Cube.cpp.obj: \
- C:/Users/12111/Desktop/VSC/SoftwareRender/Geometry/Cube.cpp \
- C:/Users/12111/Desktop/VSC/SoftwareRender/Geometry/Cube.h \
- C:/Users/12111/Desktop/VSC/SoftwareRender/math/Matrix.h \
- C:/Users/12111/Desktop/VSC/SoftwareRender/math/Vector.h \
- C:/Users/12111/Desktop/VSC/SoftwareRender/Color.h
